ffe9a2f2da50f10fc6cbe27b03e95dcb832c302d
jakeolins <jakeolins@gmail.com>
2020-07-26 16:46:46 -0500

    router fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express Yourself' });
-  res.render('', { title: 'Home' });
+  res.render('home', { title: 'Home' });
 });
 
 
